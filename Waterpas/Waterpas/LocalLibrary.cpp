e324a23dcb3015460f44d0ed150a957decb2715d
Mart Meijerink <mmjmeijerink@gmail.com>
2013-05-01 15:24:17 +0200

    .gitignore en .ino

--- Waterpas/Waterpas/LocalLibrary.cpp
+++ Waterpas/Waterpas/LocalLibrary.cpp
@@ -21,10 +21,10 @@
 #include "LocalLibrary.h"
 
 void blink(uint8_t pin, uint8_t times, uint16_t ms) {
-  for (uint8_t i=0; i<times; i++) {
-    digitalWrite(pin, HIGH); 
-    delay(ms >> 1);               
-    digitalWrite(pin, LOW);  
-    delay(ms >> 1);              
-  }
+	for (uint8_t i=0; i<times; i++) {
+		digitalWrite(pin, HIGH);
+		delay(ms >> 1);
+		digitalWrite(pin, LOW);
+		delay(ms >> 1);
+	}
 }
--- Waterpas/Waterpas/Waterpas.ino
+++ Waterpas/Waterpas/Waterpas.ino
@@ -67,35 +67,19 @@
 /// @brief	Name of the LED
 /// @details	Each board has a LED but connected to a different pin
 ///
-uint8_t myLED;
+uint8_t greenLED, lightSensor;
 
 
 ///
 /// @brief	Setup
 /// @details	Define the pin the LED is connected to
 ///
-// Add setup code 
+// Add setup code
 void setup() {
-  // myLED pin number
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega32U4__) || defined(__SAM3X8E__) // Arduino specific 
-  myLED = 13; 
-#elif defined(__PIC32MX__) // chipKIT specific
-  myLED = 13;
-#elif defined(__AVR_ATtinyX5__) // Digispark specific
-    myLED = 1; // assuming model A
-#elif defined(__AVR_ATmega644P__) // Wiring specific
-  myLED = 15; 
-#elif defined(__MSP430G2452__) || defined(__MSP430G2553__) || defined(__MSP430G2231__) || defined(__MSP430FR5739__) // LaunchPad and FraunchPad specific
-  myLED = RED_LED; 
-#elif defined(__LM4F120H5QR__) // StellarPad specific
-  myLED = RED_LED;
-#elif defined(MCU_STM32F103RB) || defined(MCU_STM32F103ZE) || defined(MCU_STM32F103CB) || defined(MCU_STM32F103RE) // Maple specific
-  myLED = BOARD_LED_PIN; 
-#elif defined(__MK20DX128__) // Teensy 3.0 specific
-    myLED = 13;
-#endif
-
-  pinMode(myLED, OUTPUT);     
+	greenLED = 11;
+	Serial.begin(9600);
+	
+	pinMode(greenLED, OUTPUT);
 }
 
 ///
@@ -104,6 +88,6 @@ void setup() {
 ///
 // Add loop code 
 void loop() {
-  blink(myLED, 3, 333);
-  delay(1000);    
+	int sensorValue = analogRead(A0);
+	analogWrite(greenLED, map(sensorValue, 0, 1023, 0, 255));
 }
