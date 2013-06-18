b75bbbe4c04b9f26aac2ae51b5f6fa457b1b74ed
Mart Meijerink <mmjmeijerink@gmail.com>
2013-06-18 15:20:41 +0200

    distance shizzle

--- Arduino/Arduino/Arduino.ino
+++ Arduino/Arduino/Arduino.ino
@@ -9,11 +9,19 @@ RF24	radio(3, 9); // Pin 3, 9, 11, 12, 13 worden door de radio gebruikt
 /// Constants
 ///
 
-typedef enum {POSSIGNAL = 0} type;
+typedef enum {POSITION = 0, DISTANCE} type;
 
 typedef struct {
 	type	msgType;
+	int		nodeId;
+	int		distance;
 } Broadcast;
+
+typedef struct {
+	int	nodeId;
+	int	currentDistance;
+	int	lastDistance;
+} GridNode;
 
 // Grid node states
 typedef enum {WAITING = 0, RADIORCV} state;
@@ -26,8 +34,10 @@ bool isGridNode = true;
 
 // Moving node vars
 int buzPin = 7;
+GridNode *gridNodes[];
 
 // Grid node vars
+int nodeId;
 state _state = WAITING;
 
 int micPort = A0;
@@ -48,19 +58,50 @@ const uint64_t broadcastPipe = 0xABCD4567EFLL;
 void sendLocSig() {
 	// Radio Broadcast - Received by all grid-nodes at the same time
 	Broadcast broadcast;
-	broadcast.msgType = POSSIGNAL;
+	broadcast.msgType = POSITION;
 	radio.stopListening();
 	radio.write(&broadcast, sizeof(broadcast));
-	radio.startListening();
 	
 	// Buzzer Signal - Received with a delay, delay between Radio and Buzzer -> Distance
-	tone(buzPin, 2750, 20);
-	delay(50);
+	tone(buzPin, 2750, 10);
+	/*delay(50);
 	tone(buzPin, 2750, 20);
 	delay(20);
 	tone(buzPin, 2750, 20);
 	delay(50);
 	tone(buzPin, 2750, 20);
+	*/
+	
+	radio.startListening();
+}
+
+void distanceToNode(msg.nodeId, msg.distance, rcvTime) {
+	GridNode node = getGridNode(msg.nodeId);
+	
+}
+
+*GridNode getGridNode(int nodeId) {
+	GridNode *result;
+	
+	bool found = false;
+	for (int i = 0; i < gridNodes.size && !found; i++) {
+		if (gridNodes[i].nodeId == nodeId) {
+			result = gridNodes[i];
+			found = true;
+		}
+	}
+	
+	if (<#condition#>) {
+		GridNode node;
+		node.nodeId = nodeId;
+		node.currentDistance = 0;
+		node.lastDistance = 0;
+		
+		gridNodes[gridNodes.size] = node;
+		result = gridNodes[gridNods.size - 1];
+	}
+	
+	return result;
 }
 
 // Grid node methods
@@ -71,15 +112,32 @@ void checkSound() {
 	
 	if (sound - average < -17 || sound - average > 17) {
 		soundRcvTime = time;
-		printf("TravelTime = %lu  \n\r", radioRcvTime - soundRcvTime);
+		sendDistance(soundRcvTime - radioRcvTime);
+		printf("TravelTime = %lu  \n\r", soundRcvTime - radioRcvTime);
 		digitalWrite(buzPin, HIGH);
 		delay(30);
 		digitalWrite(buzPin, LOW);
+		_state = WAITING;
 	}
 	
 	average = 0.95 * average + 0.05 * sound;
 	delayMicroseconds(100);
-	_state = WAITING;
+}
+
+void sendDistance(unsigned long travelTime) {
+	
+	Broadcast broadcast;
+	broadcast.msgType = DISTANCE;
+	broadcast.nodeId = nodeId;
+	broadcast.distance = distance(travelTime);
+	
+	radio.stopListening();
+	radio.write(&broadcast, sizeof(Broadcast));
+	radio.startListening();
+}
+
+int distance(unsigned long travelTime) {
+	
 }
 
 
@@ -91,8 +149,6 @@ void setup(void) {
 	Serial.begin(19200);
 	pinMode(buzPin, OUTPUT);
 	
-	// TODO: check Moving vs Grid node
-	// Mogelijke implementatie met D2 verbonden met GND
 	pinMode(2, INPUT);
 	digitalWrite(2, HIGH);
 	if (!digitalRead(2)) {
@@ -116,6 +172,8 @@ void setup(void) {
 	}
 	
 	if (isGridNode) {
+		//TODO: bereken/geef nodeId
+		
 		for (int i = 0; i < 100; i++) {
 			int sound = analogRead(micPort);
 			average = 0.95 * average + 0.05 * sound;
@@ -130,6 +188,23 @@ void setup(void) {
 void loop(void) {
 	if (!isGridNode) {
 		sendLocSig();
+		
+		if (radio.available()) {
+			unsigned long rcvTime = micros();
+			
+			Broadcast msg;
+			bool done = false;
+			while (!done) {
+				done = radio.read(&msg, sizeof(Broadcast));
+			}
+			
+			if (msg.msgType == DISTANCE) {
+				distanceToNode(msg.nodeId, msg.distance, rcvTime);
+				
+				
+			}
+		}
+		
 		delay(2000);
 	} else {
 		if (radio.available()) {
@@ -142,13 +217,17 @@ void loop(void) {
 				done = radio.read(&msg, sizeof(Broadcast));
 			}
 			
-			if (msg.msgType == POSSIGNAL) {
+			if (msg.msgType == POSITION) {
 				radioRcvTime = time;
 				_state = RADIORCV;
 			}
 		}
 		
-		if (_state == RADIORCV && micros() - radioRcvTime < 1750000) {
+		if (_state == RADIORCV) {
+			if (micros() - radioRcvTime > 100000) {
+				_state == WAITING;
+			}
+			
 			checkSound();
 		}
 	}
